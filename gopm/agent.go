fb56eb58df30de4fe71cce65171d5a94c45aaf5b
cailei <dancercl@gmail.com>
2012-09-18 15:26:22 +0800

    implement 'gopm publish'

--- gopm/agent.go
+++ gopm/agent.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "bytes"
     "fmt"
     "github.com/cailei/gopm_index/gopm_index"
     "io"
     "io/ioutil"
     "log"
     "net/http"
+    "net/url"
     "strconv"
 )
 
 var remote_db_host string = "http://localhost:8080"
 
-func agent_get_full_index_reader() io.Reader {
-    url := remote_db_host + "/all"
-    return _get_body_reader(url)
+func agent_get_full_index_reader() io.ReadCloser {
+    request := remote_db_host + "/all"
+    return _get_body_reader(request)
 }
 
 func agent_package_name_exists(name string) bool {
-    url := fmt.Sprintf("%v/name_exists?name=%v", remote_db_host, name)
-    content := _get_body_content(url)
+    request := fmt.Sprintf("%v/name_exists?name=%v", remote_db_host, name)
+    content := _get_body_content(request)
     exists, err := strconv.ParseBool(content)
     if err != nil {
         log.Fatalln(err)
@@ -29,7 +29,7 @@ func agent_package_name_exists(name string) bool {
 }
 
 func agent_upload_package(meta gopm_index.PackageMeta) {
-    url := fmt.Sprintf("%v/publish", remote_db_host)
+    request := fmt.Sprintf("%v/publish", remote_db_host)
 
     // marshal PackageMeta to json
     json, err := meta.ToJson()
@@ -37,31 +37,8 @@ func agent_upload_package(meta gopm_index.PackageMeta) {
         log.Fatalln(err)
     }
 
-    json_reader := bytes.NewReader(json)
-
     // create a POST request
-    request, err := http.NewRequest("POST", url, json_reader)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    request.ContentLength = int64(len(json))
-
-    content := _post_data(request)
-    if content == "" {
-        log.Fatalln("Unknown error!")
-    }
-
-    succ, err := strconv.ParseBool(content)
-    if err != nil {
-        log.Fatalln(err)
-    }
-
-    fmt.Print(succ)
-}
-
-func _post_data(request *http.Request) string {
-    // POST
-    response, err := http.DefaultClient.Do(request)
+    response, err := http.PostForm(request, url.Values{"pkg": {string(json)}})
     if err != nil {
         log.Fatalln(err)
     }
@@ -71,16 +48,25 @@ func _post_data(request *http.Request) string {
         log.Fatalln(response.Status)
     }
 
-    data, err := ioutil.ReadAll(response.Body)
+    body, err := ioutil.ReadAll(response.Body)
+    defer response.Body.Close()
     if err != nil {
         log.Fatalln(err)
     }
-    return string(data)
+
+    succ, err := strconv.ParseBool(string(body))
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    if !succ {
+        log.Fatalln("Unknown error!")
+    }
 }
 
-func _get_body_reader(url string) io.ReadCloser {
+func _get_body_reader(request string) io.ReadCloser {
     // GET the index content
-    response, err := http.Get(url)
+    response, err := http.Get(request)
     if err != nil {
         log.Fatalln(err)
     }
@@ -93,8 +79,10 @@ func _get_body_reader(url string) io.ReadCloser {
     return response.Body
 }
 
-func _get_body_content(url string) string {
-    body_reader := _get_body_reader(url)
+func _get_body_content(request string) string {
+    body_reader := _get_body_reader(request)
+    defer body_reader.Close()
+
     data, err := ioutil.ReadAll(body_reader)
     if err != nil {
         log.Fatalln(err)
